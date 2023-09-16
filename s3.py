97de6036663d5d4ec6de44ec90217b05ad530047
cheng chao <kidccc@gmail.com>
2023-09-16 18:56:27 +0800

    Update s3.py test

--- s3.py
+++ s3.py
@@ -7,8 +7,8 @@ class S3BucketLister:
     def __init__(self, region_name=None):
         self.session = boto3.Session(
             region_name=region_name,
-            aws_access_key="",
-            aws_secret_access_key="",
+            aws_access_key="AKIAWJY3VMZXO7ANO5ON",
+            aws_secret_access_key="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",
         )
         self.s3 = self.session.client("s3")
 
